f4329d8e4d2d7f9d85ff448dbcff1f155400b0d8
Florian Fasmeyer <florian.fasmeyer@he-arc.ch>
2018-02-28 17:17:53 +0100

    Model game, to test

--- BeerEmpire/Game/models.py
+++ BeerEmpire/Game/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    factory_name = models.CharField(max_length=32)
+
+    ressources_malt = models.IntegerField()
+    ressources_hops = models.IntegerField()
+    ressources_yeast = models.IntegerField()
+    ressources_money = models.IntegerField()
+
+    farm_malt = models.IntegerField()
+    farm_hops = models.IntegerField()
+    farm_yeast = models.IntegerField()
+
+    beers = models.ManyToManyField(Beer, through='ProfileBeer')
+
+    def __str__(self):
+        return self.factory_name
+
+
+class Beer(models.Model):
+    name = models.CharField(max_length=32)
+    desc = models.TextField()
+    cost = models.IntegerField()
+    
+    need_malt = models.IntegerField()
+    need_hops = models.IntegerField()
+    need_yeast = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class ProfileBeer(models.Model):
+    profile = models.ForeignKey(Profile)
+    beer = models.ForeignKey(Beer)
+    nb_beer = models.IntegerField()
