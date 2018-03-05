a2683747bd4e5dda4ab35ce67d739bbcdbcff8cd
Florian Fasmeyer <florian.fasmeyer@he-arc.ch>
2018-03-05 11:12:53 +0100

    admin register profileBeer for interface

--- BeerEmpire/Game/admin.py
+++ BeerEmpire/Game/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from .models import Beer, Profile
 
-admin.site.register([Beer, Profile])
+admin.site.register([Beer, Profile, ProfileBeer])
 
 # Register your models here.
