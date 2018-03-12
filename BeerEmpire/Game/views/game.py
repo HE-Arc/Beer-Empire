f9ac9354fc1f86fdc24c438b9c2de33baf4da69f
Florian Fasmeyer <florian.fasmeyer@he-arc.ch>
2018-03-12 11:32:34 +0100

    funciton views to class views

--- BeerEmpire/Game/views/game.py
+++ BeerEmpire/Game/views/game.py
@@ -3,21 +3,13 @@ from django.shortcuts import render, redirect
 from django.views import generic, View
 #from django.urls import reverse_lazy
 
-<<<<<<< HEAD
-# from.models import Beer, Profile
-
 class FarmView(generic.TemplateView):
-    template_name= "base/farm.html"
+    template_name= "game/farm.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['informations du joueur'] = "Kek"
         return context
-=======
-def farm(request):
-    context = {}
-    return render(request, 'game/farm.html', context)
-
 
 def factory(request):
     context = {}
@@ -27,4 +19,3 @@ def factory(request):
 def market(request):
     context = {}
     return render(request, 'game/market.html', context)
->>>>>>> 93024e0eef9a6baa5156698ece8b3c9143c799c2
