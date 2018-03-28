f86223198ba03d50a00d3d7e69032d9dfdb81ec6
axel.bentodas <axel.bentodasilva@he-arc.ch>
2018-03-28 17:01:26 +0200

    Add div in market/factory part

--- BeerEmpire/Game/views/base.py
+++ BeerEmpire/Game/views/base.py
@@ -4,3 +4,8 @@ from django.contrib.auth import logout
 def index(request):
     context = {}
     return render(request, 'registration/login.html', context)
+
+
+def register(request):
+    context = {}
+    return render(request, 'registration/register.html', context)
