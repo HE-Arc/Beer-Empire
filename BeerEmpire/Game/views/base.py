e56126b92a342b3f0d2ee8a308ff9e563bdd6ae5
axel.bentodas <axel.bentodasilva@he-arc.ch>
2018-03-28 17:47:33 +0200

    Add registration page

--- BeerEmpire/Game/views/base.py
+++ BeerEmpire/Game/views/base.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render
 from django.contrib.auth import logout
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views import generic
+from django.views.generic import View
+from ..form import UserForm
 
 def index(request):
     context = {}
     return render(request, 'registration/login.html', context)
 
 
-def register(request):
-    context = {}
-    return render(request, 'registration/register.html', context)
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('/farm')
+
+        return render(request, self.template_name, {'form': form})
