cd97a4140ecc58d21f7620bc9f9b5f6dc21d150d
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2018-04-05 16:55:02 +0200

    Factory working, using db

--- BeerEmpire/Game/views/game.py
+++ BeerEmpire/Game/views/game.py
@@ -7,7 +7,7 @@ from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
-from Game.models import Profile
+from Game.models import Profile, Beer
 
 class GameViewBase(LoginRequiredMixin, generic.TemplateView):
     login_url = '/login/'
@@ -32,6 +32,7 @@ class FactoryView(GameViewBase):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["beers"] = Beer.objects.all()
         return context
 
 
